c01c92d24b8285a0830f0fffc01056b8135df3b0
wolfather <so.israelweb@gmail.com>
2025-02-10 12:23:06 -0300

    update view async data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { map } from 'rxjs';
+import { map, Subscription } from 'rxjs';
 import { GetDataService } from './services/get-data.service';
 import { ApiUserResponse } from './entities/apiResponses';
 
@@ -17,9 +17,11 @@ import { ApiUserResponse } from './entities/apiResponses';
 export class AppComponent implements OnInit, OnDestroy {
   data$: ApiUserResponse[] = [];
   #getDataService = new GetDataService();
+  #fetchErrorCalled = 0;
+  #subscription = new Subscription()
 
   ngOnInit() {
-    this.#getDataService
+    this.#subscription.add(this.#getDataService
       .getData<ApiUserResponse[]>('users')
       .pipe(
         map((data) =>
@@ -33,10 +35,33 @@ export class AppComponent implements OnInit, OnDestroy {
         next: (res) => {
           this.data$ = res
         }
-      });
+      }));
+  }
+
+  fetchOnError() {
+    this.#subscription.add(this.#getDataService
+      .getData<ApiUserResponse[]>('users')
+      .pipe(
+        map((data) =>
+          data.map((item) => ({
+            ...item,
+            email: item.email.toLowerCase(),
+          }))
+        )
+      )
+      .subscribe({
+        next: (res) => {
+          this.data$ = res
+        },
+        error: () => {
+          this.#fetchErrorCalled += 1;
+        },
+        
+      }));
   }
 
   ngOnDestroy() {
     this.data$ = [];
+    this.#subscription.unsubscribe();
   }
 }
